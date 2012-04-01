e207ef3443737957e3465b57f9a860ca553c640c
Kevin J. Lynagh <kevin@keminglabs.com>
2012-04-01 13:03:18 -0700

    Clean up project.clj.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject cljx "0.1.0-SNAPSHOT"
+(defproject com.keminglabs/cljx "0.1.0-SNAPSHOT"
 
   :description "Static Clojure code rewriting"
   :url "http://github.com/lynaghk/cljx"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/BSD-3-Clause"}
-  
-  :dependencies [[org.clojure/tools.namespace "0.1.2"]
-                 [org.clojure/core.logic "0.7.0"]
+
+  :dependencies [[org.clojure/core.logic "0.7.0"]
                  [jonase/kibit "0.0.3"]]
-  
+
   :eval-in-leiningen true)
+
