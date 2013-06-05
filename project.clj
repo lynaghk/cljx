3cb37f024e14a1a855a650704d376fdfd91ea4f0
Chas Emerick <chas@cemerick.com>
2013-06-05 06:47:10 -0400

    revert attempt to track core.logic rev requested by kibit (re: gh-9)

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/BSD-3-Clause"}
 
-  :dependencies [[jonase/kibit "0.0.8"]
+  :dependencies [[org.clojure/core.logic "0.7.0"]
+                 [jonase/kibit "0.0.8"
+                  ; TODO this can't be good, but using newer core.logic revs
+                  ; results in a stackoverflow....
+                  ; https://github.com/lynaghk/cljx/issues/9
+                  :exclusions [org.clojure/core.logic]]
                  [watchtower "0.1.1"]]
 
   :min-lein-version "2.0.0"
