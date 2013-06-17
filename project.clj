8ad0b712bb7a00d720c49f429ca5f1e26d89aabb
Kevin J. Lynagh and Chas Emerick <ubuntu@ip-10-254-24-242.us-west-2.compute.internal>
2013-06-17 17:39:24 +0000

    WIP: replace core.logic with a custom walk across cgrand's sjacket.

--- project.clj
+++ project.clj
@@ -1,24 +1,18 @@
-(defproject com.keminglabs/cljx "0.2.2"
+(defproject com.keminglabs/cljx "0.3.0-SNAPSHOT"
 
   :description "Static Clojure code rewriting"
   :url "http://github.com/lynaghk/cljx"
   :license {:name "BSD"
             :url "http://www.opensource.org/licenses/BSD-3-Clause"}
 
-  :dependencies [[org.clojure/core.logic "0.7.0"]
-                 [jonase/kibit "0.0.8"
-                  ; TODO this can't be good, but using newer core.logic revs
-                  ; results in a stackoverflow....
-                  ; https://github.com/lynaghk/cljx/issues/9
-                  :exclusions [org.clojure/core.logic]]
-                 [watchtower "0.1.1"]
-                 [com.cemerick/piggieback "0.0.4"]]
+  :dependencies [[org.clojure/core.match "0.2.0-beta2"]
+                 [org.clojars.trptcolin/sjacket "0.1.0.3"]
+                 [com.cemerick/piggieback "0.0.4"]
+                 [watchtower "0.1.1"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [org.clojure/clojurescript "0.0-1820"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
                                                      cljx.repl-middleware/wrap-cljx]}}}
 
-  :min-lein-version "2.0.0"
-  :eval-in-leiningen true)
-
+  :eval-in :leiningen)
--- scratch.clj
+++ scratch.clj
@@ -1,184 +1,40 @@
 (ns scratch
-  (:use [cljx.core :only [generate]]
-        [cljx.rules :only [cljs-rules clj-rules]]
-        :reload-all))
+  (:require [net.cgrand.sjacket :as sj]
+            [net.cgrand.sjacket.parser :as p]
+            [clojure.core.match :refer [match]]
+            [clojure.zip :as z]))
 
-(do
-  (def cljx-path "test/cljx")
-  (def clj-output-path "test/generated/clj")
-  (def cljs-output-path "test/generated/cljs"))
+(def grr
+  "^:clj  (test 1 2)
+ ^:cljs (test 2 3)")
 
-(do
-  (def cljx-path "../c2/src/cljx")
-  (def clj-output-path "../c2/.generated/clj")
-  (def cljs-output-path "../c2/.generated/cljs"))
-
-(comment
-  (set! *print-meta* false)
-  (use 'clojure.stacktrace)
-
-  (do
-    (generate cljx-path cljs-output-path "cljs" cljs-rules)
-    (generate cljx-path clj-output-path "clj" clj-rules))
-
-  (use '[clojure.java.io :only [reader]])
-  
-  (let [r (reader (java.io.File. "test/cljx/testns/core.cljx"))]
-    (kibit.check/check-reader r
-                              :rules  cljs-rules
-                              :guard identity
-                              :resolution :toplevel))
-
-  
-  
-  (use '[kibit.core :only [simplify simplify-one]])
-  (use 'clojure.core.logic)
+(def parsed (p/parser grr))
 
 
+(defn- node-language
+  [node]
+  (match [node]
+    ;;[{:tag :meta :content ["^" {:tag :keyword :content [":" {:tag :name :content ["cljx"]}]}]}]
 
-  (defn simplify-one [expr rules]
-    (let [alt (run* [q]
-                    (fresh [pat subst]
-                           (membero [pat subst] rules)
-                           (project [pat subst]
-                                    (all (pat expr)
-                                         (subst q)))))]
-      (if (empty? alt) expr (first alt))))
+    [{:tag :meta :content [_ {:tag :keyword :content [_ {:content [lang]}]}]}] (keyword lang)
+    :else nil))
 
-  (def form '(defmacro nom [x] (some (delicious)) forms))
-  
-  ;;this works nicely
-  (let [x    (lvar)
-        rest (lvar)]
-    (run* [q]  
-          (== form (llist 'defmacro x rest))
-          (== q x)))
-  ;;=> (nom)
-
-  ;;why not this?
-  (let [x    (lvar)
-        rest (lvar)]
-    (run* [q]  
-          (== form (defmacro ~x . ~rest))
-          (== q x)))
-  ;;=> ()
-
-  ;;This doesn't work either
-  (run* [q]
-        (prep '(all
-                (== form '(defmacro ?x . ?rest))
-                (== q ?x))))
-  ;;=> java.lang.ClassCastException: clojure.lang.PersistentList cannot be cast to clojure.lang.IFn
-  
-  
-  ;;This also works
-  (run* [q]
-        (fresh [pat alt]
-               (membero [pat alt]
-                        [(prep '[(defmacro ?x . ?rest)
-                                 ?x])])
-               (== pat form)
-               (== q alt)))
-  ;;=> (nom) 
-
-
-
-  (run* [q]
-        (all
-         (== form (prep '(defmacro ?x . ?rest)))
-         (== q true)))
-
-  (clojure.core/doall
-   (clojure.core.logic/solve false [q]
-                             (all
-                              (== form (prep (quote (defmacro ?x . ?rest))))
-                              (== q true))))
-
-
-  (clojure.core/doall
-   (clojure.core.logic/solve false [q]
-                             (eval (prep (quote (all
-                                                 (== form (defmacro ?x . ?rest))
-                                                 (== q true)))))))
-  
-  (run* [q]
-        (prep '(all
-                 (== form (defmacro ?x . ?rest))
-                 (== q true))))
-;;why not this?
-  (run* [q]
-        (fresh [pat alt]
-               (membero [pat alt]
-                        [(prep '[#(== % (defmacro ?x . ?rest))
-                                 #(== % ?x)])])
-               (pat form)
-               (alt q)))
-        ;;=> clojure.core.logic.LVar cannot be cast to clojure.lang.IFn
-        
-  
+(defn walk
+  [language-target node]
+  (if (z/branch? node)
+    (z/node (map walk (z/children node)))
+    (when (= language-target (node-language node))
+      node)))
 
 
 
 
 
-  
+(defn elide
+  [target code]
+  (-> (p/parser code)
 
-  (simplify-one form
-                [(fresh [pat alt]
-                        (== [pat alt]
-                            (prep '[(defmacro ?x . ?rest) #(== % ?x)]))
-                        [#(== pat %)
-                         alt])])
+      
+      sj/str-pt))
 
 
-
-
-
-
-
-
-
-
-
-
-
-  (simplify-one form
-                [[#(matche [%]
-                           ([['defmacro . _]]))
-                  #(== % :exclude)]])
-
-
-
-
-  (def inc-rule
-    (let [x (lvar)]
-      [#(all (== % `(~'+ ~x 1))
-             ;; It's easy to add pattern predicates
-             (pred x number?))
-       #(== % `(inc ~x))]))
-
-
-
-  
-  (let [x    (lvar)
-        rest (lvar)]
-    `(~'defmacro ~x . ~rest)
-    )
-
-
-  (defn ns->cljs-ns
-    "Creates a cljs-compatible namespace form by splitting vars marked with ^:macro into appropriate use-macros or require-macros forms"
-    [ns-form]
-
-    (let [use-forms]
-
-      )
-    )
-
-  (def ns-form '(ns
-                    (:use [my-ns.thing :only [a b c ^:macro a-macro]])
-                  ))
-
-  (require '[clojure.core.logic :as logic])
-
-  
