8579a06edab8bfcfa4502ca1c7b14fdeebcfa31e
Julien Eluard <julien.eluard@gmail.com>
2013-08-19 14:25:34 -0300

    Make sure test task detects generated test files.

--- src/cljx/hooks.clj
+++ src/cljx/hooks.clj
@@ -2,14 +2,22 @@
   (:require leiningen.cljx
             [robert.hooke :as hooke]
             [leiningen.core.project :as project]
-            [leiningen.compile :as lcompile]))
+            [leiningen.compile :as lcompile]
+            [leiningen.test :as ltest]))
+
+(def ^:private hooked (promise))
 
 (defn- hook [task & args]
   ; leiningen.jar/jar unmerges all default profiles before calling through to
   ; e.g. leiningen.compile/compile.  No idea why, but cljx needs to find its
   ; dependency vector, which may be in a profile...
-  (leiningen.cljx/cljx (project/merge-profiles (first args) [:default]))
+  (when (not (realized? hooked))
+    (deliver hooked true)
+    (leiningen.cljx/cljx (project/merge-profiles (first args) [:default])))
   (apply task args))
 
 (defn activate []
-  (hooke/add-hook #'lcompile/compile #'hook))
+  (hooke/add-hook #'lcompile/compile #'hook)
+  ;;Also hook test to workaround https://github.com/technomancy/leiningen/issues/1300
+  ;;File generation will happen only once.
+  (hooke/add-hook #'ltest/test #'hook))
