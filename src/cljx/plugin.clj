c68de816dd8403025154650313e34ab2db672b93
Hugo Duncan <hugo@hugoduncan.org>
2014-08-30 09:04:35 -0400

    Add named profiles
    Use named profiles to prevent cljx dependencies leaking into jar.
    
    Fixes #47

--- src/cljx/plugin.clj
+++ src/cljx/plugin.clj
@@ -1,5 +1,6 @@
 (ns cljx.plugin
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [leiningen.core.project :refer [add-profiles set-profiles]]))
 
 (def ^:private cljx-version
   (-> (io/resource "META-INF/leiningen/com.keminglabs/cljx/project.clj")
@@ -11,12 +12,29 @@
         (str "Something went wrong, version of cljx is not a string: "
              cljx-version))
 
+(def profiles
+  {:cljx/inject-dependencies
+   {:dependencies [['com.keminglabs/cljx cljx-version :scope "test"]]}
+   :cljx/inject-nrepl-middleware
+   {:repl-options
+    {:nrepl-middleware
+     '[cljx.repl-middleware/wrap-cljx cemerick.piggieback/wrap-cljs-repl]}}})
+
+(defn add-to-default-profile
+  [project profiles]
+  (vary-meta project update-in [:profiles :default] (fnil into []) profiles))
+
 (defn middleware
   [project]
-  (-> project
-      (update-in [:dependencies]
-                 (fnil into [])
-                 [['com.keminglabs/cljx cljx-version]])
-      (update-in [:repl-options :nrepl-middleware]
-                 (fnil into [])
-                 '[cljx.repl-middleware/wrap-cljx cemerick.piggieback/wrap-cljs-repl])))
+  (if (-> project meta ::middleware-applied) ; guard recursion via set-profiles
+    project
+    (-> project
+        (vary-meta assoc ::middleware-applied true)
+        (add-profiles profiles)
+        (add-to-default-profile [:cljx/inject-dependencies
+                                 :cljx/inject-nrepl-middleware])
+        (set-profiles
+         (into (-> project meta :included-profiles distinct)
+               [:cljx/inject-dependencies
+                :cljx/inject-nrepl-middleware]))
+        (vary-meta dissoc ::middleware-applied))))
