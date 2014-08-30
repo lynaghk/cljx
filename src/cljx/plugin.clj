2676bc8145187e295986741b4665b1f91378af55
Hugo Duncan <hugo@hugoduncan.org>
2014-08-30 13:52:08 -0400

    Use a single profile

--- src/cljx/plugin.clj
+++ src/cljx/plugin.clj
@@ -13,10 +13,9 @@
              cljx-version))
 
 (def profiles
-  {:cljx/inject-dependencies
-   {:dependencies [['com.keminglabs/cljx cljx-version :scope "test"]]}
-   :cljx/inject-nrepl-middleware
-   {:repl-options
+  {:cljx/inject
+   {:dependencies [['com.keminglabs/cljx cljx-version :scope "test"]]
+    :repl-options
     {:nrepl-middleware
      '[cljx.repl-middleware/wrap-cljx cemerick.piggieback/wrap-cljs-repl]}}})
 
@@ -31,10 +30,7 @@
     (-> project
         (vary-meta assoc ::middleware-applied true)
         (add-profiles profiles)
-        (add-to-default-profile [:cljx/inject-dependencies
-                                 :cljx/inject-nrepl-middleware])
+        (add-to-default-profile [:cljx/inject])
         (set-profiles
-         (into (-> project meta :included-profiles distinct)
-               [:cljx/inject-dependencies
-                :cljx/inject-nrepl-middleware]))
+         (into (-> project meta :included-profiles distinct) [:cljx/inject]))
         (vary-meta dissoc ::middleware-applied))))
