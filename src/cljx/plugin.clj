8b820cea2aaef0a932f0c7f255d7c38e148741c0
Chas Emerick <chas@cemerick.com>
2014-09-05 12:26:09 -0400

    only add cljx as a runtime dependency when running a REPL, fixes gh-47

--- src/cljx/plugin.clj
+++ src/cljx/plugin.clj
@@ -1,22 +1,29 @@
 (ns cljx.plugin
-  (:require [clojure.java.io :as io]))
+  (:require [robert.hooke :as hooke]
+            leiningen.repl
+            [clojure.java.io :as io]))
 
-(def ^:private cljx-version
-  (-> (io/resource "META-INF/leiningen/com.keminglabs/cljx/project.clj")
-       slurp
-       read-string
-       (nth 2)))
+(def cljx-coordinates
+  (-> (or (io/resource "META-INF/leiningen/com.keminglabs/cljx/project.clj")
+        (io/resource  "META-INF/leiningen/org.clojars.cemerick/cljx/project.clj"))
+    slurp
+    read-string
+    ((fn [[_ artifact version]] [artifact version]))))
 
-(assert (string? cljx-version)
-        (str "Something went wrong, version of cljx is not a string: "
-             cljx-version))
+(assert (and (symbol? (first cljx-coordinates))
+          (string? (second cljx-coordinates)))
+  (str "Something went wrong, cljx coordinates are invalid: "
+    cljx-coordinates))
 
 (defn middleware
   [project]
-  (-> project
-      (update-in [:dependencies]
-                 (fnil into [])
-                 [['com.keminglabs/cljx cljx-version]])
+  (if (or (-> project :cljx :disable-repl-integration)
+        (not (-> project meta :included-profiles set (contains? :repl))))
+    project
+    (-> project
       (update-in [:repl-options :nrepl-middleware]
-                 (fnil into [])
-                 '[cljx.repl-middleware/wrap-cljx cemerick.piggieback/wrap-cljs-repl])))
+        (fnil into [])
+        '[cljx.repl-middleware/wrap-cljx cemerick.piggieback/wrap-cljs-repl])
+      (update-in [:dependencies]
+        (fnil conj [])
+        cljx-coordinates))))
